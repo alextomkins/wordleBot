0916a0cc89261aad1b14abfd80905c8021bf3776
alextomkins <72971625+alextomkins@users.noreply.github.com>
2022-02-10 23:46:06 +1000

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+ 
+
+driver = webdriver.Chrome()
+
+url = ''
+driver.get(url)
