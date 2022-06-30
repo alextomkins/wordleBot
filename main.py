c0b58e5a8d89be826ca4a7e499fa9da2db2ee6e1
Alex Tomkins <72971625+alextomkins@users.noreply.github.com>
2022-07-01 04:29:57 +1000

    Complete rework of guessing algorithm. Currently only one guess implimented and double letters are not handled

--- main.py
+++ main.py
@@ -2,22 +2,11 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.chrome.options import Options
 import time
-import re
 
 def main():
     setup()
 
-    returnClues()
-
-    k=input("press close to exit") 
-
-    #Solve next 5 guesses
-    #for i in range(5):
-    #    returnClues()
-    #    removeAbsent()
-    #    removeNear()
-    #    removeMatches()
-    #    method()
+    print(wordList)
 
 def setup():
     global driver
@@ -46,7 +35,9 @@ def setup():
     with open('words.txt', 'r') as f:
         wordList = f.read().split()
 
-    wordGuess('SALET')
+    wordGuess('salet')
+
+    returnClues(guessNumber)
 
 
 
@@ -54,9 +45,7 @@ def wordGuess(wordToGuess):
     elem.send_keys(wordToGuess)
     elem.send_keys(Keys.ENTER)
 
-def returnClues():
-    global guessNumber
-    
+def returnClues(guessNumber):
     time.sleep(2)
 
     letterList = []
@@ -72,117 +61,48 @@ def returnClues():
     print(letterList)
     print(attributeList)
 
-    guessNumber = guessNumber + 1
+    guessNumber += 1
+
+    reduceWordList(letterList, attributeList)
 
 
-def removeAbsent():
-    for index in range(len(wordList)):
-        if absent[0] in wordList[index] or absent[1] in wordList[index] or absent[2] in wordList[index] or absent[3] in wordList[index] or absent[4] in wordList[index] or absent[5] in wordList[index] or absent[6] in wordList[index] or absent[7] in wordList[index] or absent[8] in wordList[index] or absent[9] in wordList[index] or absent[10] in wordList[index] or absent[11] in wordList[index] or absent[12] in wordList[index] or absent[13] in wordList[index] or absent[14] in wordList[index] or absent[15] in wordList[index] or absent[16] in wordList[index] or absent[17] in wordList[index] or absent[18] in wordList[index] or absent[19] in wordList[index] or absent[20] in wordList[index] or absent[21] in wordList[index] or absent[22] in wordList[index] or absent[23] in wordList[index] or absent[24] in wordList[index] or absent[25] in wordList[index] or absent[26] in wordList[index] or absent[27] in wordList[index] or absent[28] in wordList[index] or absent[29] in wordList[index]:
-            wordList[index] = ''
+def reduceWordList(letterList, attributeList):
+    for i in range(len(letterList)):
+        if attributeList[i] == 'absent':
+            index = 0
+            while(True):
+                if letterList[i].lower() in wordList[index]:
+                    del wordList[index]
+                    index -= 1
 
-def removeNear():
-    numNear = 5
+                if index >= len(wordList) - 1:
+                    break
 
-    if nearmatches[4] == '/':
-        numNear = 4
+                index += 1
 
-    if nearmatches[3] == '/':
-        numNear = 3
+        if attributeList[i] == 'present':
+            index = 0
+            while(True):
+                if letterList[i].lower() == wordList[index][i] or letterList[i].lower() not in wordList[index]:
+                    del wordList[index]
+                    index -=1
 
-    if nearmatches[2] == '/':
-        numNear = 2
+                if index >= len(wordList) - 1:
+                    break
 
-    if nearmatches[1] == '/':
-        numNear = 1
+                index += 1
 
-    if nearmatches[0] == '/':
-        numNear = 0
+        if attributeList[i] == 'correct':
+            index = 0
+            while(True):
+                if letterList[i].lower() != wordList[index][i]:
+                    del wordList[index]
+                    index -=1
 
-    for index in range(len(wordList)):
-        if numNear == 5:
-            if nearmatches[0] not in wordList[index] or nearmatches[1] not in wordList[index] or nearmatches[2] not in wordList[index] or nearmatches[3] not in wordList[index] or nearmatches[4] not in wordList[index]:
-                wordList[index] = ''
+                if index >= len(wordList) - 1:
+                    break
 
-        if numNear == 4:
-            if nearmatches[0] not in wordList[index] or nearmatches[1] not in wordList[index] or nearmatches[2] not in wordList[index] or nearmatches[3] not in wordList[index]:
-                wordList[index] = ''
-
-        if numNear == 3:
-            if nearmatches[0] not in wordList[index] or nearmatches[1] not in wordList[index] or nearmatches[2] not in wordList[index]:
-                wordList[index] = ''
-
-        if numNear == 2:
-            if nearmatches[0] not in wordList[index] or nearmatches[1] not in wordList[index]:
-                wordList[index] = ''
-
-        if numNear == 1:
-            if nearmatches[0] not in wordList[index]:
-                wordList[index] = ''
-
-def removeMatches():
-    global numMatch
-    numMatch = 5
-
-    if matches[4] == '/':
-        numMatch = 4
-
-    if matches[3] == '/':
-        numMatch = 3
-
-    if matches[2] == '/':
-        numMatch = 2
-
-    if matches[1] == '/':
-        numMatch = 1
-
-    if matches[0] == '/':
-        numMatch = 0
-
-    for index in range(len(wordList)):
-
-        if numMatch == 4:
-            if matches[0] not in wordList[index] or matches[1] not in wordList[index] or matches[2] not in wordList[index] or matches[3] not in wordList[index]:
-                wordList[index] = ''
-
-        if numMatch == 3:
-            if matches[0] not in wordList[index] or matches[1] not in wordList[index] or matches[2] not in wordList[index]:
-                wordList[index] = ''
-
-        if numMatch == 2:
-            if matches[0] not in wordList[index] or matches[1] not in wordList[index]:
-                wordList[index] = ''
-
-        if numMatch == 1:
-            if matches[0] not in wordList[index]:
-                wordList[index] = ''
-
-def method():
-    global wordList
-    global numMatch
-
-    wordList = list(filter(None,wordList))
-
-    nextGuess = False
-    doubleLetter = False
-
-    nextGuess2 = '/'
-
-    for i in wordList:
-        doubleLetter = False
-        for letters in range(5):
-            if i.count(i[letters]) > 1:
-                doubleLetter = True
-            if letters == 4 and doubleLetter == False and nextGuess == False:
-                nextGuess2 = i
-                nextGuess = True
-
-    if nextGuess2 == '/' and numMatch != 5:
-        nextGuess2 = wordList[0]
-
-    wordGuess(nextGuess2)
-
-    if numMatch != 5:
-        wordList.remove(nextGuess2)
+                index += 1
 
 if __name__ == "__main__":
     main()
