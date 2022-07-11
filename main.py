b046ec9afc9c8f64a0235654faf98848c8618862
Alex Tomkins <72971625+alextomkins@users.noreply.github.com>
2022-07-12 03:54:41 +1000

    Code adapted to fit python naming conventions, bug fixes for multiple letter clues and general cleanup

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
+#-----Description----------------------------------------------------#
+#
+#  WORDLE BOT, Version 2 (12.07.2022)
+#
+#  This program provides an automated solution to solving the daily 
+#  wordle puzzle. The Google Chrome WebDriver is utilized to access
+#  the official Wordle game on the New York Times website where the
+#  script enters guesses until the answer is found, or it fails.
+#  The answer is found through eliminating words from a list by
+#  using the clues returned from the website. When the script 
+#  finishes, the reduced word list is printed to the console. 
+#
+#  Author: Alexander Tomkins 
+#  github.com/alextomkins
+#
+#--------------------------------------------------------------------#
+
+# Import necessary selenium functions
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+
+# Import time functions
 import time
 
+# Define a class to allow driver, elem, word_guess, and words to always be accessible
 class WordList:
+    # Initialise words list and navigate the website to allow for guessing
     def __init__(self):
         with open('words.txt', 'r') as f:
             self.words = f.read().split()
         
-        self.guessNumber = 1
-        self.wordToGuess = 'salet'
+        self.guess_number = 1
+        self.word_to_guess = 'salet'
 
         options = webdriver.ChromeOptions()
         options.add_experimental_option('excludeSwitches', ['enable-logging'])
@@ -24,72 +46,105 @@ class WordList:
 
         self.elem.click()
 
-        self.wordGuess()
+        self.word_guess()
 
-    ###########################
+    #-----Conditional Statements------------------------------------------#
 
-    def correctNumberOfMultiple(self, index, letterList, multiAttributes, i):
-        if self.words[index].count(letterList[i]) != (multiAttributes.count('correct') + multiAttributes.count('present')):
+    def correct_number_multiple(self, index, letter_list, multi_attributes, i):
+        if self.words[index].count(letter_list[i].lower()) != (multi_attributes.count('correct') + multi_attributes.count('present')):
             return True
         
         return False
 
-    def absentOrPresentMultiple(self, index, letterList, attributeList, i):
-        if (attributeList[i] == 'absent' or attributeList[i] == 'present') and letterList[i].lower() == self.words[index][i]:
-            return True
-
-        return False
-
-    def isCorrect(self, index, letterList, attributeList, i):
-        if attributeList[i] == 'correct' and letterList[i].lower() != self.words[index][i]:
+    def less_than_number_multiple(self, index, letter_list, multi_attributes, i):
+        if (self.words[index].count(letter_list[i]) < (multi_attributes.count('correct') + multi_attributes.count('present'))):
             return True
         
         return False
 
-    #######################
+    def absent_present_multiple(self, index, letter_list, attribute_list, i):
+        if (attribute_list[i] == 'absent' or attribute_list[i] == 'present') and letter_list[i].lower() == self.words[index][i]:
+            return True
 
-    def returnClues(self):
+        return False
+
+    def is_correct(self, index, letter_list, attribute_list, i):
+        if attribute_list[i] == 'correct' and letter_list[i].lower() != self.words[index][i]:
+            return True
+        
+        return False
+
+    def game_finished(self, attribute_list):
+        if attribute_list.count('correct') == 5:
+            return True
+        
+        return False
+
+    def all_absent(self, multi_attributes):
+        if multi_attributes.count('absent') == len(multi_attributes):
+            return True
+        
+        return False
+
+    def all_present(self, multi_attributes):
+        if multi_attributes.count('present') == len(multi_attributes):
+            return True
+        
+        return False
+
+    def all_correct(self, multi_attributes):
+        if multi_attributes.count('correct') == len(multi_attributes):
+            return True
+        
+        return False
+
+    #--------------------------------------------------------------------#
+
+    # Return absent, present or correct for letters in the most recent guess
+    def return_clues(self):
         time.sleep(2)
 
-        letterList = []
-        attributeList = []
+        letter_list = []
+        attribute_list = []
 
         for i in range(5):
-            letter = self.driver.find_element(By.XPATH, f'/html/body/div/div[1]/div/div[{self.guessNumber}]/div[{i+1}]/div') 
-            attribute = self.driver.find_element(By.XPATH, f'/html/body/div/div[1]/div/div[{self.guessNumber}]/div[{i+1}]/div').get_attribute("data-state")
+            letter = self.driver.find_element(By.XPATH, f'/html/body/div/div[1]/div/div[{self.guess_number}]/div[{i+1}]/div') 
+            attribute = self.driver.find_element(By.XPATH, f'/html/body/div/div[1]/div/div[{self.guess_number}]/div[{i+1}]/div').get_attribute("data-state")
 
-            letterList.append(letter.text)
-            attributeList.append(attribute)
+            letter_list.append(letter.text)
+            attribute_list.append(attribute)
 
-        self.guessNumber += 1
+        self.guess_number += 1
 
-        if attributeList.count('correct') == 5:
+        if self.game_finished(attribute_list):
             pass
         else:
-            self.reduceWordList(letterList, attributeList)
+            self.reduce_word_list(letter_list, attribute_list)
     
-    def wordGuess(self):
-        self.elem.send_keys(self.wordToGuess)
+    # Send word guess to the website
+    def word_guess(self):
+        self.elem.send_keys(self.word_to_guess)
         self.elem.send_keys(Keys.ENTER)
-        self.returnClues()
+        self.return_clues()
 
-    def reduceWordList(self, letterList, attributeList):
+    # Reduce word list to ensure remaining words follow clues read from return_clues()
+    def reduce_word_list(self, letter_list, attribute_list):
         indicies = []
 
-        for i in range(len(letterList)):
+        for i in range(len(letter_list)):
 
-            indicies.append([j for j, x in enumerate(letterList) if x == letterList[i]])
+            indicies.append([j for j, x in enumerate(letter_list) if x == letter_list[i]])
 
-            multiAttributes = []
+            multi_attributes = []
 
             if len(indicies[i]) > 1:
                 for k in range(len(indicies[i])):
-                    multiAttributes.append(attributeList[indicies[i][k]])
+                    multi_attributes.append(attribute_list[indicies[i][k]])
 
-                if multiAttributes.count('absent') == len(multiAttributes):
+                if self.all_absent(multi_attributes):
                     index = 0
                     while(True):
-                        if self.letterList[i].lower() in self.words[index]:
+                        if letter_list[i].lower() in self.words[index]:
                             del self.words[index]
                             index -= 1
 
@@ -98,10 +153,10 @@ class WordList:
 
                         index += 1
 
-                if multiAttributes.count('present') == len(multiAttributes):
+                if self.all_present(multi_attributes):
                     index = 0
                     while(True):
-                        if letterList[i].lower() == self.words[index][i] or letterList[i].lower() not in self.words[index]:
+                        if letter_list[i].lower() == self.words[index][i] or letter_list[i].lower() not in self.words[index]:
                             del self.words[index]
                             index -=1
 
@@ -110,10 +165,10 @@ class WordList:
 
                         index += 1
 
-                if multiAttributes.count('correct') == len(multiAttributes):
+                if self.all_correct(multi_attributes):
                     index = 0
                     while(True):
-                        if letterList[i].lower() != self.words[index][i]:
+                        if letter_list[i].lower() != self.words[index][i]:
                             del self.words[index]
                             index -=1
                         
@@ -122,10 +177,10 @@ class WordList:
 
                         index += 1
 
-                if 'absent' in multiAttributes and 'present' in multiAttributes and 'correct' in multiAttributes:
+                if 'absent' in multi_attributes and 'present' in multi_attributes and 'correct' in multi_attributes:
                     index = 0
                     while(True):
-                        if self.correctNumberOfMultiple(index, letterList, multiAttributes, i) and self.absentOrPresentMultiple(index, letterList, attributeList, i) and self.isCorrect(index, letterList, attributeList, i):
+                        if self.correct_number_multiple(index, letter_list, multi_attributes, i) or self.absent_present_multiple(index, letter_list, attribute_list, i) or self.is_correct(index, letter_list, attribute_list, i):
                             del self.words[index]
                             index -=1
 
@@ -134,10 +189,10 @@ class WordList:
 
                         index += 1
 
-                if 'absent' in multiAttributes and 'present' in multiAttributes and 'correct' not in multiAttributes:
+                if 'absent' in multi_attributes and 'present' in multi_attributes and 'correct' not in multi_attributes:
                     index = 0
                     while(True):
-                        if self.correctNumberOfMultiple(index, letterList, multiAttributes, i) and self.absentOrPresentMultiple(index, letterList, attributeList, i):
+                        if self.correct_number_multiple(index, letter_list, multi_attributes, i) or self.absent_present_multiple(index, letter_list, attribute_list, i):
                             del self.words[index]
                             index -=1
 
@@ -146,10 +201,10 @@ class WordList:
 
                         index += 1
 
-                if 'absent' in multiAttributes and 'correct' in multiAttributes and 'present' not in multiAttributes:
+                if 'absent' in multi_attributes and 'correct' in multi_attributes and 'present' not in multi_attributes:
                     index = 0
                     while(True):
-                        if self.correctNumberOfMultiple(index, letterList, multiAttributes, i) and self.absentOrPresentMultiple(index, letterList, attributeList, i) and self.isCorrect(index, letterList, attributeList, i):
+                        if self.correct_number_multiple(index, letter_list, multi_attributes, i) or self.absent_present_multiple(index, letter_list, attribute_list, i) or self.is_correct(index, letter_list, attribute_list, i):
                             del self.words[index]
                             index -=1
 
@@ -158,10 +213,10 @@ class WordList:
 
                         index += 1
 
-                if 'present' in multiAttributes and 'correct' in multiAttributes and 'absent' not in multiAttributes:
+                if 'present' in multi_attributes and 'correct' in multi_attributes and 'absent' not in multi_attributes:
                     index = 0
                     while(True):
-                        if self.words[index].count(self.letterList[i]) < (multiAttributes.count('correct') + multiAttributes.count('present')) and self.absentOrPresentMultiple(index, self.letterList, self.attributeList, i) and self.isCorrect(index, self.letterList, self.attributeList, i):
+                        if self.less_than_number_multiple(self, index, letter_list, multi_attributes, i) or self.absent_present_multiple(index, letter_list, attribute_list, i) or self.is_correct(index, letter_list, attribute_list, i):
                             del self.words[index]
                             index -=1
 
@@ -171,10 +226,10 @@ class WordList:
                         index += 1
 
             else:
-                if attributeList[i] == 'absent':
+                if attribute_list[i] == 'absent':
                     index = 0
                     while(True):
-                        if letterList[i].lower() in self.words[index]:
+                        if letter_list[i].lower() in self.words[index]:
                             del self.words[index]
                             index -= 1
 
@@ -183,10 +238,10 @@ class WordList:
 
                         index += 1
 
-                if attributeList[i] == 'present':
+                if attribute_list[i] == 'present':
                     index = 0
                     while(True):
-                        if letterList[i].lower() == self.words[index][i] or letterList[i].lower() not in self.words[index]:
+                        if letter_list[i].lower() == self.words[index][i] or letter_list[i].lower() not in self.words[index]:
                             del self.words[index]
                             index -=1
 
@@ -195,10 +250,10 @@ class WordList:
 
                         index += 1
 
-                if attributeList[i] == 'correct':
+                if attribute_list[i] == 'correct':
                     index = 0
                     while(True):
-                        if letterList[i].lower() != self.words[index][i]:
+                        if letter_list[i].lower() != self.words[index][i]:
                             del self.words[index]
                             index -=1
                         
@@ -207,7 +262,11 @@ class WordList:
 
                         index += 1
         
-        self.wordToGuess = self.words[0]
-        self.wordGuess()
-        
+        self.word_to_guess = self.words[0]
+        self.word_guess()
+
+# Start the script by initialising a wordList object        
 w1 = WordList()
+
+# Print reduced word list, first item will be correct guess or next guess if the bot failed
+print(w1.words)
